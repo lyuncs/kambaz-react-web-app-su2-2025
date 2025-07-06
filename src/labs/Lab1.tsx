459b13cc0c49298985a965ff2d10a302356275fc
lyuncs <li.yun6@northeastern.edu>
2025-07-06 11:37:11 +0800

    second commit

---
+++ src/labs/Lab1.tsx
@@ -0,0 +1,9 @@
+export default function Lab1() {
+  return (
+    <div>
+      <h3>Lab 1</h3>
+      <p>Welcome to Lab 1!</p>
+      <p>This is a simple lab component.</p>
+    </div>
+  );
+}
---
+++ src/labs/Lab2.tsx
@@ -0,0 +1,10 @@
+export default function Lab2() {
+  return (
+    <div>
+      <h3>Lab 2</h3>
+      <p>Lets learn about CSS</p>
+      <p>This lab introduces more complex component.</p>
+      <p>Stay tuned for more features</p>
+    </div>
+  );
+}
